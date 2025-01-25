464b522b2e11023157e009d152fea4256a77fff7
Danny Perkins <dperkins3pi@gmail.com>
2025-01-24 22:25:27 -0700

    Adjust file to make it save videos

--- VisualInspection.py
+++ VisualInspection.py
@@ -5,17 +5,26 @@ ECEn-631 Visual Inspection Project created by Harrison Garrett in 2020
 
 import cv2 as cv
 import numpy as np
+import os
 '''
 Set WEBCAM to 1 to use your webcam or 0 to use the Flea2 cameras on the lab machine
 Set CATCHER to 1 to use the catcher connected to the lab machine or 0 to use your own computer
 '''
 WEBCAM = 1
 CATCHER = 0
+
 if WEBCAM:
     camera = cv.VideoCapture(0)
 else:
     from src.Flea2Camera2 import FleaCam
     camera = FleaCam()
+
+if not camera.isOpened():
+    print("Error: Could not open webcam")
+    exit()
+width = int(camera.get(cv.CAP_PROP_FRAME_WIDTH))
+height = int(camera.get(cv.CAP_PROP_FRAME_HEIGHT))
+videout = cv.VideoWriter('./Video.avi', cv.VideoWriter_fourcc(*'XVID'), 25, (width, height))  # Video format
 
 while True:
     # Get Opencv Frame
@@ -27,8 +36,19 @@ while True:
     # print(cv_image.shape)
     cv.imshow('frame',frame)
 
-
     # Press Q on keyboard to  exit
     if cv.waitKey(25) & 0xFF == ord('q'):
-      break
-# ssh dbp52@ssh.et.byu.edu
+        break
+
+    videout.write(frame)    
+
+camera.release()
+    
+# # ssh dbp52@ssh.et.byu.edu:~/VisualInspection/Video.avi
+# # source ecen_venv/bin/activate
+
+# # Features to Use
+#     # Color
+#     # LBP
+#     # Size/Area
+#     # Shape somehow
